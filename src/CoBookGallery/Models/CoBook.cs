2a01099f871fd8f5e6568aa76ae567e8a79c393a
1Victory <Living4E23@aol.com>
2016-07-08 18:42:25 -0400

    Added Co Book and Artist Models

---
+++ src/CoBookGallery/Models/CoBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoBookGallery.Models
+{
+    public class CoBook
+    {
+        public int Id { get; set; }
+        public string SeriesTItle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml{ get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+    }
+}

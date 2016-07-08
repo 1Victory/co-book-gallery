ed9639426d085a68ad3b6dbfbaa800d12cc575a7
1Victory <Living4E23@aol.com>
2016-07-07 23:52:34 -0400

    Updated Co Book Detail View

--- src/CoBookGallery/Controllers/CoBooksController.cs
+++ src/CoBookGallery/Controllers/CoBooksController.cs
@@ -10,7 +10,23 @@ namespace CoBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            return View();
+           
+
+
+            ViewBag.SeriesTitle = "The Amazing Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>This is where information will about the c book will go..";
+            ViewBag.Artists = new string[]
+                {
+                    "Script: Dan Slott",
+                    "Pencils: Humberto Ramos",
+                    "Inks: Victor Olazaba",
+                    "Colors: Edgar Delgado",
+                    "Letters: Chris Eliopoulos"
+
+                };
+
+                return View();
 
         }
     }
